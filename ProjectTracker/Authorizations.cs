ae586971a9d47534f5257dd3339b947d3c56eefb
sszpak <srszpak@gmail.com>
2018-08-24 15:29:21 -0500

    removed usings not being used

--- ProjectTracker/Authorizations.cs
+++ ProjectTracker/Authorizations.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Security.Cryptography;
-using System.Text;
 using System.Threading.Tasks;
 using System.DirectoryServices.AccountManagement;
 
--- ProjectTracker/Projects.cs
+++ ProjectTracker/Projects.cs
@@ -1,8 +1,5 @@
 ﻿using ProjectTracker.DAL;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjectTracker
